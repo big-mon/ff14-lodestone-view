2abe03288d2e45fbe874b2af317af082e720c590
big-mon <mrmon93@gmail.com>
2019-12-17 07:47:28 +0900

    ・全体のJSをネイティブからjQueryに変更 ・フォルダ構造を整理

---
+++ js/ff14-detail.js
@@ -0,0 +1,41 @@
+// 個別ページの場合、JSを実行
+$(document).ready(function() {
+  var url = location.href;
+  if (url.indexOf("lodestone/character") != -1) {
+    imgConvertBigger();
+  }
+});
+
+function imgConvertBigger() {
+  // 画像要素のリストを加工(リッチ版)
+  hrefEdit($(".blog__area .img_box"));
+
+  // 画像要素のリストを加工(シンプル版)
+  hrefEdit($(".blog__area--simple .thumb_list .mb10"));
+  $(".blog__area--simple .thumb_list").each(function(index, item) {
+    $(item).removeClass("thumb_list");
+  });
+}
+
+// hrefを入れ替える関数
+function hrefEdit(list) {
+  $(list).each(function(index, item) {
+    // 規定タグを変更
+    $(item)
+      .addClass("custom_img")
+      .removeClass("img_box");
+
+    // 画像リンクを取得
+    var href = $(item)
+      .find("a")
+      .attr("href");
+
+    // 規定要素を変更
+    $(item)
+      .find("img")
+      .removeAttr("width")
+      .removeAttr("height")
+      .removeAttr("style")
+      .attr("src", href);
+  });
+}
