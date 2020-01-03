cc568fffde9c7c465c66849b304186de5d1ec517
big-mon <mrmon93@gmail.com>
2020-01-03 21:22:19 +0900

    ポップアップ画面のコードを整理 ・名前空間を定義 ・重複表示となる場合、重複分は非表示とするよう修正

--- popup/blocklist.js
+++ popup/blocklist.js
@@ -1,77 +1,90 @@
-// ブロックリスト
-var list;
+var BLOCK_FUNCTION = {
+  // ブロックリスト
+  list: [],
+
+  // ブロックリストを更新(対象を全件投入)
+  updateList: function(list) {
+    chrome.storage.sync.set({ blocklist: list }, function() {
+      console.log("saved");
+    });
+  },
+
+  // ブロックリストを取得
+  getList: function() {
+    chrome.storage.sync.get(["blocklist"], function(result) {
+      var tmp = result.blocklist != null ? result.blocklist : [];
+
+      BLOCK_FUNCTION.list = tmp
+        // chromeから取得したリストをソートして格納(world, name)
+        .sort(function(a, b) {
+          a1 = a.world.toString().toLowerCase();
+          b1 = b.world.toString().toLowerCase();
+          if (a1 < b1) return -1;
+          else if (a1 > b1) return 1;
+          else {
+            if (a.name < b.name) return -1;
+            else if (a.name > b.name) return 1;
+            else return 0;
+          }
+        })
+        // 重複を除外(id)
+        .filter(function(v1, i1, a1) {
+          return (
+            a1.findIndex(function(v2) {
+              return v1.id === v2.id;
+            }) === i1
+          );
+        });
+
+      // 画面に反映
+      BLOCK_FUNCTION.redrawList();
+    });
+  },
+
+  // ブロックリストを画面に反映
+  redrawList: function() {
+    // ブロックリストのHTMLを生成
+    var html = [];
+    $(BLOCK_FUNCTION.list).each(function(index, item) {
+      var tmp =
+        '<tr data-id="' +
+        item.id +
+        '" data-name="' +
+        item.name +
+        '" data-world="' +
+        item.world +
+        '"><td><a href="https://jp.finalfantasyxiv.com/lodestone/character/' +
+        item.id +
+        '/" target="_blank" rel="noopener">' +
+        item.name +
+        "</a></td><td>" +
+        item.world +
+        '</td><td><span class="remove">解除</span></td></tr>';
+      html.push(tmp);
+    });
+
+    // ブロックリストのHTMLを更新
+    $("#blocklist-list tbody").html(html);
+  },
+
+  // 設定画面からブロックリストを取得
+  readyUpdate: function() {
+    var tmp = [];
+    $("#blocklist-list tbody tr").each(function(index, item) {
+      tmp.push({
+        id: $(item).data("id"),
+        name: $(item).data("name"),
+        world: $(item).data("world")
+      });
+    });
+    BLOCK_FUNCTION.list = tmp;
+  }
+};
 
 // ページ読み込み完了時に実行
 window.onload = function() {
-  getBlocklist();
+  BLOCK_FUNCTION.getList();
 };
-
-// ブロックリストを更新(対象を全件投入)
-function setBlocklist(list) {
-  chrome.storage.sync.set({ blocklist: list }, function() {
-    console.log("saved");
-  });
-}
-
-// ブロックリストを取得
-function getBlocklist() {
-  chrome.storage.sync.get(["blocklist"], function(result) {
-    // chromeから取得したリストをソートして格納(world, name)
-    list = result.blocklist.sort(function(a, b) {
-      a1 = a.world.toString().toLowerCase();
-      b1 = b.world.toString().toLowerCase();
-      if (a1 < b1) return -1;
-      else if (a1 > b1) return 1;
-      else {
-        if (a.name < b.name) return -1;
-        else if (a.name > b.name) return 1;
-        else return 0;
-      }
-    });
-
-    // 画面に反映
-    refreshList();
-  });
-}
-
-// 設定画面からブロックリストを取得
-function getBlocklistFromTable() {
-  var tmp = [];
-  $("#blocklist-list tbody tr").each(function(index, item) {
-    tmp.push({
-      id: $(item).data("id"),
-      name: $(item).data("name"),
-      world: $(item).data("world")
-    });
-  });
-  list = tmp;
-}
-
-// ブロックリストを画面に反映
-function refreshList() {
-  // ブロックリストのHTMLを生成
-  var html = [];
-  $(list).each(function(index, item) {
-    var tmp =
-      '<tr data-id="' +
-      item.id +
-      '" data-name="' +
-      item.name +
-      '" data-world="' +
-      item.world +
-      '"><td><a href="https://jp.finalfantasyxiv.com/lodestone/character/' +
-      item.id +
-      '/" target="_blank" rel="noopener">' +
-      item.name +
-      "</a></td><td>" +
-      item.world +
-      '</td><td><span class="remove">解除</span></td></tr>';
-    html.push(tmp);
-  });
-
-  // ブロックリストのHTMLを更新
-  $("#blocklist-list tbody").html(html);
-}
 
 // 解除ボタン押下
 $(document).on("click", ".remove", function() {
@@ -83,10 +96,10 @@ $(document).on("click", ".remove", function() {
 // 保存ボタン押下
 $(document).on("click", "#save", function() {
   // 残るブロックリストを取得
-  getBlocklistFromTable();
+  BLOCK_FUNCTION.readyUpdate();
 
   // ブロックリストを更新
-  setBlocklist(list);
+  BLOCK_FUNCTION.updateList(BLOCK_FUNCTION.list);
 });
 
 // キャンセルボタン押下
